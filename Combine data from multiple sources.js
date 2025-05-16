03f29a21658f7ef503000b7622048af65c9ca905
abdullahsher <abdullahhswat1234@gmail.com>
2025-05-16 15:27:33 +0500

    Use cases of spread operator.

---
+++ Combine data from multiple sources.js
@@ -0,0 +1,7 @@
+// combine data from different sources 
+const salesData = [100, 200];
+const marketingData = [50, 80];
+
+const totalData = [...salesData, ...marketingData];
+
+console.log(totalData); 
