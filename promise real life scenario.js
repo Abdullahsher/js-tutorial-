8f94b72bff47b1aae95f2461568f285a09a94c24
abdullahsher <abdullahhswat1234@gmail.com>
2025-05-17 16:38:59 +0500

    Promises real life scenario.

---
+++ promise real life scenario.js
@@ -0,0 +1,23 @@
+function orderFood(food) {
+    let mypromise = new Promise((resolve, reject) => {
+    console.log("You have ordered the food");
+
+    setTimeout(() => {
+      let isAvailable = Math.random() < 0.5;
+
+      if (isAvailable) {
+        resolve(` ${food} is delivered.`);
+      } else {
+        reject(`Sorry, ${food} is unavailable.`);
+      }
+    }, 2000);
+  });
+
+  return mypromise;
+}
+
+
+orderFood("Pharta")
+  .then(result => console.log("Your's", result))
+  .catch(error => console.log("There is an error ", error))
+  .finally(() => console.log(" Your order is processed."));
